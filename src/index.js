57ad2875e8d5400f0017ea42e879fc6f639678f8
Kevin Gil <93kevingil@gmail.com>
2023-10-05 15:10:18 -0300

    fe file structure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import App from './components/App.jsx';
 
 const container = document.getElementById('app');
 const root = createRoot(container);
-root.render(<h1>Hello, world</h1>);
+root.render(<App />);
